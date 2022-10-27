5b5c140a747cbd3f47ef72c635822251a53b4558
jaswant2111058 <jaswant2111058@akgec.ac.in>
2022-10-27 21:01:50 +0530

    New One

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("OK..., request received");
+});
+
+app.listen(8000, function () {
+  console.log("Server is up.");
+});
--- jassi.js
+++
@@ -1,1 +0,0 @@
-console.log('jassi is my name')
